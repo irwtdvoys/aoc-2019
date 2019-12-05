3fb8111af692b726c63eac49ff18caa20182855d
Steve Adams <irwtdvoys@gmail.com>
2019-12-05 13:52:36 +0000

    Replace instruction pointer with memory cursor

--- app/Intcode.php
+++ app/Intcode.php
@@ -1,24 +1,31 @@
 <?php
 	namespace App;
-	
+
+	use App\Intcode\Instruction;
+	use App\Intcode\Parameter;
 	use Bolt\Files;
-	
+	use Exception;
+
 	class Intcode
 	{
-		private $memory;
-		private $instructionPointer = 0;
-		private $stopped = false;
-		
-		public function load($filename = "input.txt")
+		public array $memory = array();
+		public bool $stopped = false;
+		public int $cursor = 0;
+
+		public function load($filename = "input.txt"): void
 		{
 			$this->setProgram((new Files())->load($filename));
 		}
-		
-		public function getInstruction(int $address = 0)
+
+		public function nextInstruction(): Instruction
 		{
-			$offset = $address * 4;
-			
-			return new Instruction(array_slice($this->memory, $offset, 4));
+			// send 4 long memory chunk from cursor (max used by instructions, only required)
+			$instruction = new Instruction(array_slice($this->memory, $this->cursor, 4));
+
+			// Move cursor dynamic amount based on number of memory locations used by the instruction
+			$this->cursor += count($instruction->parameters) + 1;
+
+			return $instruction;
 		}
 		
 		public function processInstruction(Instruction $instruction)
@@ -33,28 +40,35 @@
 					break;
 				case 99:
 					$this->stopped = true;
-					$this->output();
+					break;
+				default:
+					throw new Exception("Unknown opcode [" . $instruction->opcode . "]");
 					break;
 			}
 		}
-		
-		public function run()
+
+		public function run(): string
 		{
+			$count = 0;
+
 			while (!$this->stopped)
 			{
-				$operation = $this->getInstruction($this->instructionPointer);
-				$this->processInstruction($operation);
-				
-				$this->instructionPointer++;
+				$instruction = $this->nextInstruction();
+
+				$this->processInstruction($instruction);
+
+				$count++;
 			}
+
+			return $this->output();
 		}
 
 		public function output(): string
 		{
 			return $this->memory[0];
 		}
-		
-		public function setProgram($string)
+
+		public function setProgram($string): void
 		{
 			$this->memory = array_map(function ($element) {
 				return (int)$element;
