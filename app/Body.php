d325a85baa3b78b425c23f9e026db2a0da37f245
Steve Adams <irwtdvoys@gmail.com>
2019-12-06 13:45:05 +0000

    Add classes for building tree + calculating outputs

---
+++ app/Body.php
@@ -0,0 +1,27 @@
+<?php
+
+	namespace App;
+
+	class Body
+	{
+		public string $name;
+		public array $links;
+		public int $orbits;
+		public ?string $parent;
+
+		public function __construct(string $name)
+		{
+			$this->name = $name;
+			$this->links = array();
+			$this->orbits = 0;
+			$this->parent = null;
+		}
+
+		public function conatins($name)
+		{
+			$data = json_encode($this->links);
+
+			return (strpos($data, $name) !== false) ? true : false;
+		}
+	}
+?>
---
+++ app/OrbitalTransfers.php
@@ -0,0 +1,111 @@
+<?php
+
+	namespace App;
+
+	use Bolt\Files;
+
+	class OrbitalTransfers
+	{
+		public array $bodies = array();
+		public array $orbits = array();
+
+		public array $data = array();
+
+		public function load(string $override = null)
+		{
+			$data = (!isset($override)) ? (new Files())->load(ROOT . "data/06") : $override;
+
+			$lines = explode(PHP_EOL, trim($data));
+
+			foreach ($lines as $line)
+			{
+				$orbit = explode(")", $line);
+				$this->orbits[] = $orbit;
+				$this->bodies = array_unique(array_merge($this->bodies, $orbit));
+			}
+		}
+
+		public function calculateOrbitCounts(&$node, $level = 0)
+		{
+			$node->orbits = $level;
+
+			if (count($node->links))
+			{
+				foreach ($node->links as &$link)
+				{
+					$this->calculateOrbitCounts($link, $level + 1);
+				}
+			}
+		}
+
+		public function totalOrbits($bodies)
+		{
+			$count = 0;
+
+			foreach ($bodies as $next)
+			{
+				$count += $next->orbits;
+			}
+
+			return $count;
+		}
+
+		public function orbitalChanges($from, $to)
+		{
+			$from = $this->data[$from];
+			$to = $this->data[$to];
+			$current = $from;
+
+			for ($orbit = $from->orbits; $orbit > 0; $orbit--)
+			{
+				$isRoot = $current->conatins($to->name);
+
+				if ($isRoot === true)
+				{
+					break;
+				}
+
+				$current = $this->data[$current->parent];
+			}
+
+			return ($from->orbits - $current->orbits) + ($to->orbits - $current->orbits) - 2;
+		}
+
+		public function buildTree()
+		{
+			foreach ($this->bodies as $body)
+			{
+				$this->data[$body] = new Body($body);
+			}
+
+			// create tree
+			foreach ($this->orbits as $orbit)
+			{
+				$this->data[$orbit[0]]->links[] = &$this->data[$orbit[1]];
+				$this->data[$orbit[1]]->parent = &$this->data[$orbit[0]]->name;
+			}
+
+			$this->calculateOrbitCounts($this->data['COM']);
+		}
+
+		public function run($part = 1): int
+		{
+			$this->buildTree();
+
+			$result = 0;
+
+			switch ($part)
+			{
+				case 1:
+					$result = $this->totalOrbits($this->data);
+					break;
+				case 2:
+					$result = $this->orbitalChanges("YOU", "SAN");
+					break;
+
+			}
+
+			return $result;
+		}
+	}
+?>
