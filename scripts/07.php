3acdb0a9dcb4809b269d82e0885494b916663ca7
Steve Adams <irwtdvoys@gmail.com>
2019-12-07 19:08:07 +0000

    Day 7 script

---
+++ scripts/07.php
@@ -0,0 +1,12 @@
+<?php
+	define("ROOT", __DIR__ . "/../");
+
+	include_once(ROOT . "bin/init.php");
+
+	use App\Amplifiers;
+
+	$helper = new Amplifiers();
+	$result = $helper->run([5, 6, 7, 8, 9]); // [0, 1, 2, 3, 4]
+
+	echo($result . PHP_EOL);
+?>
