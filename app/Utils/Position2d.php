06441acc3667c687aec5f8fd96a2fd5006fc9f6f
Steve Adams <irwtdvoys@gmail.com>
2019-12-13 11:53:23 +0000

    Add 2D position utility class

---
+++ app/Utils/Position2d.php
@@ -0,0 +1,15 @@
+<?php
+	namespace App\Utils;
+
+	class Position2d
+	{
+		public int $x = 0;
+		public int $y = 0;
+
+		public function __construct(int $x = 0, int $y = 0)
+		{
+			$this->x = $x;
+			$this->y = $y;
+		}
+	}
+?>
