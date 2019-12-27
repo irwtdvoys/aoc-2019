34d52470995c294c3cc0212f44e6ede018832d09
Steve Adams <irwtdvoys@gmail.com>
2019-12-27 12:31:35 +0000

    Parameterise all data sent to instruction
    No longer need to check for the required length

--- app/Intcode/VM/Instruction.php
+++ app/Intcode/VM/Instruction.php
@@ -1,44 +1,19 @@
 <?php
 	namespace App\Intcode\VM;
 
-	use Exception;
-
 	class Instruction
 	{
 		public int $opcode;
 		public array $parameters = array();
 
-		public function __construct($data) // data may be too much, only parameterise required amount
+		public function __construct($data)
 		{
 			list($modes, $this->opcode) = $this->extract($data[0], 2);
 
-			switch ($this->opcode)
-			{
-				case 1:
-				case 2:
-				case 7:
-				case 8:
-					$count = 3;
-					break;
-				case 3:
-				case 4:
-				case 9:
-					$count = 1;
-					break;
-				case 5:
-				case 6:
-					$count = 2;
-					break;
-				case 99:
-					$count = 0;
-					break;
-				default:
-					throw new Exception("Missing parameter count for opcode [" . $this->opcode . "]");
-					break;
-			}
+			$count = count($data);
 
 			// Add parameters to instruction
-			for ($index = 1; $index <= $count; $index++)
+			for ($index = 1; $index < $count; $index++)
 			{
 				list($modes, $mode) = $this->extract($modes);
 
